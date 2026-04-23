57f5510fea6f8120c1182f7f96e8811069bfcd1b
juninho-2105 <junior.rsampai0@outlook.com>
2026-04-23 00:55:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -321,7 +321,7 @@ else:
         st.info("Carrinho vazio")
 
 # ==================== 6. GESTÃO DE DESPESAS (CUSTOS FIXOS/VARIÁVEIS) ====================
-    elif menu == "💸 Despesas":
+elif menu == "💸 Despesas":
     st.header("💸 Registro de Custos e Despesas")
     st.info("Lance aqui custos como aluguel, energia, reposição de estoque ou manutenção.")
 
